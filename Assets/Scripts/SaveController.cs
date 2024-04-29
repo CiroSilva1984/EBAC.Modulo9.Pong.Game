c846c2c1d917c4842d7d0b50601fbef2fadca9fe
Bowie <cirosilv@gmail.com>
2024-04-29 20:10:00 -0300

    Resetando o Jogo

--- Assets/Scripts/SaveController.cs
+++ Assets/Scripts/SaveController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveController : MonoBehaviour
 {
@@ -64,5 +65,11 @@ public class SaveController : MonoBehaviour
     public string GetLastWinner()
     {
         return PlayerPrefs.GetString(saveWinnerKey);
+    }
+
+    public void ClearSave()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
