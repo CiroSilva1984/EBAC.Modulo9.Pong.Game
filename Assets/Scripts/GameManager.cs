cdf4f4440a2d1710291f34537cec28903c2e4a3c
Bowie <cirosilv@gmail.com>
2024-04-29 13:41:48 -0300

    Pong Game Módulo 9

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform playerPaddle;
+    public Transform enemyPaddle;
+
+    public int scorePlayer = 0;
+    public int scoreEnemy = 0;
+
+    public TextMeshProUGUI textPointPlayer;
+    public TextMeshProUGUI textPointEnemy;
+
+    public BallController ballController;
+
+    public int winPoints = 3;
+
+    private void Start()
+    {
+        ResetGame();
+    }
+
+    void ResetGame()
+    {
+        //Defini as posições iniciais das Raquetes
+        playerPaddle.position = new Vector3(7f, 0f, 0f);
+        enemyPaddle.position = new Vector3(-7f, 0f, 0f);
+
+        ballController.ResetBall();
+
+        scorePlayer = 0;
+        scoreEnemy = 0;
+
+        textPointPlayer.text = scorePlayer.ToString();
+        textPointEnemy.text = scoreEnemy.ToString();
+    }
+
+    public void ScorePlayer()
+    {
+        scorePlayer++;
+        textPointPlayer.text = scorePlayer.ToString();
+        CheckWin();
+    }
+    public void ScoreEnemy()
+    {
+        scoreEnemy++;
+        textPointEnemy.text = scoreEnemy.ToString();
+        CheckWin();
+    }
+
+    public void CheckWin()
+    {
+        if(scoreEnemy >= winPoints || scorePlayer >= winPoints)
+        {
+            ResetGame();
+        }
+    }
+}
